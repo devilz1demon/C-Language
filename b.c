60017ea72fd3f9a7c5f608c4a52f0b3cbf0be196
devilz1demon <devilzassassin@gmail.com>
2017-05-01 13:58:07 +0530

    Main Commit

---
+++ b.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	float f,a;
+	printf("Enter the Number to determine the square root:\n");
+	scanf("%f",&f);
+	a=sqrt(f);	
+	printf("The Square Root Of %f is %f \n",f,a);
+}
